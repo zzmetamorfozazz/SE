0c4448b0eef452c7cea45df1af46743550f647f8
Furick <Furick@speed>
2017-12-23 23:42:01 +0200

    HomeWork01

--- Jane01/src/com/gmail/qa/ko/ya/Main.java
+++ Jane01/src/com/gmail/qa/ko/ya/Main.java
@@ -1,9 +1,21 @@
 package com.gmail.qa.ko.ya;
 
-public class Main 
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("test");
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int firstNumb = num / 10000;
+		int secondNumb = num % 10000 / 1000;
+		int thirdNumb = num % 10000 % 1000 / 100;
+		int fourthNumb = num % 10000 % 1000 % 100 / 10;
+		int fifthNumb = num % 10000 % 1000 % 100 % 10 / 1;
+		System.out.println(firstNumb);
+		System.out.println(secondNumb);
+		System.out.println(thirdNumb);
+		System.out.println(fourthNumb);
+		System.out.println(fifthNumb);
 	}
 }
