3cda32287f31c016df7f77b866313b208d54c302
Furick <Furick@speed>
2017-12-23 22:32:32 +0200

    fix

--- Jane01/src/com/gmail/qa/ko/ya/Main.java
+++ Jane01/src/com/gmail/qa/ko/ya/Main.java
@@ -1,9 +1,9 @@
 package com.gmail.qa.ko.ya;
 
-public class HomeWork {
-
-	public static void main(String[] args) {
-	
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("test");
 	}
-
 }
