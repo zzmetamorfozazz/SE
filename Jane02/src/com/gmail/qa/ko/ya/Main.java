353f6fd8557ab15afe8630083dfcdac5631363c0
Furick <Furick@speed>
2017-12-24 00:29:07 +0200

    HomeWork02

---
+++ Jane02/src/com/gmail/qa/ko/ya/Main.java
@@ -0,0 +1,23 @@
+package com.gmail.qa.ko.ya;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.println("Введите сторону А");
+		double a = sc.nextDouble();
+		System.out.println("Введите сторону B");
+		double b = sc.nextDouble();
+		System.out.println("Введите сторону C");
+		double c = sc.nextDouble();
+		double p = (a + b + c) / 2;
+		double x = p * ((p - a) * (p - b) * (p - c));
+		double s = Math.sqrt(x);
+		System.out.println("S треугольника:" + s);
+	}
+
+}
